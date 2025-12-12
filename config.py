712c2a7890e569b57b205051dd00e07878ee13bc
festimii <festimi-rrezoni@hotmail.com>
2025-12-12 10:09:34 +0100

    TEST

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ DB_USER = os.getenv("DB_USER", "festim.beqiri")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "Festimeliza123")
 
 API_USERNAME = os.getenv("API_USERNAME", "admin")
-API_PASSWORD = os.getenv("API_PASSWORD", "change_me")
-API_SECRET_KEY = os.getenv("API_SECRET_KEY", "super-secret-key")
+API_PASSWORD = os.getenv("API_PASSWORD", "API_TEST")
+API_SECRET_KEY = os.getenv("API_SECRET_KEY", "LKk100mbviva5589")
 API_TOKEN_EXPIRE_MINUTES = int(os.getenv("API_TOKEN_EXPIRE_MINUTES", "60"))
 
 CONNECTION_STRING = (
